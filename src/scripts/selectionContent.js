b935bf158c40a87859624bd4e1f00266ea61d5fc
Arto Laine <arto.laine@gmail.com>
2021-10-08 21:15:22 +0200

    Store highlight texts from page

--- src/scripts/selectionContent.js
+++ src/scripts/selectionContent.js
@@ -11,3 +11,36 @@ if (selectionString) {
 }
 
 console.log('selectionText', selection, selectionString);
+if (selection && selection.type === 'Range') {
+    let selectedText = selection.focusNode.nodeValue.substring(selection.baseOffset, selection.focusOffset);
+    console.log('selected text is: ', selectedText);
+    let data = null;
+    // let location = window.location.host + window.location.pathname
+    let location = window.location.href;
+
+    chrome.storage.sync.get(location, (result) => {
+        console.log('Data currently is ', result);
+        if (result) {
+            data = result;
+        }
+    });
+
+    if (!data) {
+        data = {};
+        data[location] = {
+            highlights: []
+        };
+    }
+
+    console.log('location data', data[location]);
+    if (!data[location].highlights.includes(selectedText)) {
+        data[location].highlights.push(selectedText);
+        console.log('New data: ', data);
+
+        chrome.storage.sync.set(data, function() {
+            console.log('Value is set');
+        });
+    } else {
+        console.log('Data already exists');
+    }
+}
